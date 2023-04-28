0b72d8806130e6f6bfe604f21513be199de2033a
Hempfest <64885225+Hempfest@users.noreply.github.com>
2023-04-27 20:12:17 -0700

    * Merged gift command into the main messaging command. * Added more messaging.

--- pelican-bukkit/src/main/java/com/github/sanctum/messenger/bukkit/command/MessageCommand.java
+++ pelican-bukkit/src/main/java/com/github/sanctum/messenger/bukkit/command/MessageCommand.java
@@ -1,27 +1,39 @@
 package com.github.sanctum.messenger.bukkit.command;
 
+import com.github.sanctum.labyrinth.LabyrinthProvider;
 import com.github.sanctum.labyrinth.formatting.FancyMessage;
 import com.github.sanctum.labyrinth.formatting.completion.SimpleTabCompletion;
 import com.github.sanctum.labyrinth.formatting.completion.TabCompletionIndex;
+import com.github.sanctum.labyrinth.library.CompostElement;
+import com.github.sanctum.labyrinth.library.InventorySync;
+import com.github.sanctum.labyrinth.library.ItemCompost;
+import com.github.sanctum.labyrinth.library.ItemMatcher;
+import com.github.sanctum.labyrinth.library.ItemSync;
+import com.github.sanctum.labyrinth.library.StringUtils;
 import com.github.sanctum.messenger.api.DeliverySequence;
 import com.github.sanctum.messenger.api.PelicanAPI;
 import com.github.sanctum.messenger.api.entity.Pelican;
+import com.github.sanctum.messenger.api.entity.Recipient;
 import com.github.sanctum.messenger.util.PelicanUtility;
+import com.github.sanctum.panther.container.PantherCollection;
+import com.github.sanctum.panther.container.PantherList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 public class MessageCommand extends Command {
 	public MessageCommand() {
 		super("message");
-		setAliases(Arrays.asList("msg", "tell", "t", "mail"));
+		setAliases(Arrays.asList("msg", "gift", "send", "tell", "t", "mail"));
 	}
 
 	private final PelicanAPI api = PelicanAPI.getInstance();
@@ -45,7 +57,8 @@ public class MessageCommand extends Command {
 			if (base == null) return true;
 
 			if (args.length == 0) {
-
+				newMessage().then("&fUsage: &c/" + commandLabel + " <playerNames,..> [message]").send(p).queue();
+				newMessage().then("&fLeaving the message field blank will gift the specified player(s) the item in your hand.").send(p).queue();
 				return true;
 			}
 
@@ -54,6 +67,40 @@ public class MessageCommand extends Command {
 					boolean n = !base.isManualRead();
 					base.setManualRead(n);
 					newMessage().then("&bAutomatic message reading set to &f" + (!n)).send(p).queue();
+				}
+				PantherCollection<Pelican> entities = new PantherList<>();
+				for (String user : args[0].split(",")) {
+					Pelican pelican = api.getPelican(user, Pelican.Type.BUKKIT);
+					if (pelican != null) {
+						entities.add(pelican);
+					}
+				}
+
+				if (!entities.isEmpty()) {
+					ItemStack item = p.getInventory().getItemInMainHand();
+					if (entities.size() > item.getAmount()) {
+						// not enough of the item to share with this many people.
+						newMessage().then("&cYou don't have enough of this item to share with people.").send(p).queue();
+						return true;
+					}
+					if (entities.size() == 1) {
+						Recipient r = entities.get(0);
+						DeliverySequence sequence = base.write(new ItemStack(item), "Gift", r).complete();
+						sequence.deploy();
+						newMessage().then("&3You sent item").then(" ").then("&f[&dHOVER&f]").hover(item).then(" ").then("&3to user " + r.getName()).send(p).queue();
+						item.setAmount(0);
+					} else {
+						ItemStack copy = new ItemStack(item);
+						newMessage().then("&3You sent item").then(" ").then("&f[&dHOVER&f]").hover(copy).then(" ").then("&3to &e" + entities.size() + " &3user(s)").send(p).queue();
+						for (Recipient r : entities) {
+							copy.setAmount(1);
+							DeliverySequence sequence = base.write(copy, "Gift", r).complete();
+							sequence.deploy();
+							item.setAmount(item.getAmount() - 1);
+						}
+					}
+				} else {
+					newMessage().then("&cNot able to match the provided player(s) from the database.").send(p).queue();
 				}
 				return true;
 			}
@@ -75,4 +122,5 @@ public class MessageCommand extends Command {
 		}
 		return true;
 	}
+
 }
